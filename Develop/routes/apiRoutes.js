acc2f9051e183a3a1f1e1701ce2377ec803e2d36
Hare-Ball <robby.hare@gmail.com>
2021-07-22 09:41:53 -0500

    updates to apiRoutes and server to accomodate apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,36 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const express = require('express')
+const fs = require("fs")
 const path = require('path');
+const app = express();
 
-router.get('/api/notes',(req, res)=>{
-    
-}
+function writeNotes(note) {
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), note, (err) => {
+      if (err) throw err;
+      console.log("Your note has been saved.");
+    });
+  }
+
+  function readNotes() {
+    fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, note) => {
+      if (err) throw err;
+      console.log(notes);
+      return note;
+    });
+  }
+
+  module.exports = (app) => {
+
+    app.get("/api/notes", (res, req)=> {
+        const notes = readNotes();
+        res.json(notes);
+    });
+
+    app.post("/api/notes", (res, req) => {
+        readNotes().then((notes) => {
+            notes.push(req.body);
+            writeNotes(notes);
+            res.json(req.body);
+        });
+  });
+};
--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const htmlRoutes = require('./routes/htmlRoutes')
-const app = express()
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes');
+const app = express();
 
- app.use(express.json())
- app.use(express.urlencoded({
-     extended:true,
- }))
-app.use(express.static('public'))
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended:true,
+}));
+
+app.use(express.static('public'));
+
+app.use(apiRoutes);//added apiroutes above html
 
 app.use(htmlRoutes)//call apis above html
 
- const port = 3001;
- app.listen(port,()=>{
-     console.log("listening on port: ", port)
- })
+const port = 3001;
+app.listen(port,()=>{
+    console.log("listening on port: ", port)
+});
