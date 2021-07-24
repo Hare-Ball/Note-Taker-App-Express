2074232f70077913ee9131d14d7d5592b40cd923
Hare-Ball <robby.hare@gmail.com>
2021-07-24 14:31:06 -0500

    updates to remove duplicate api route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,16 +4,16 @@ const path = require('path');
 
 var jsonData = JSON.parse(fs.readFileSync(path.join(__dirname,'../db/db.json')))
 
-router.get('/api/notes', (req,res)=> {
+router.get('/notes', (req,res)=> {
   console.log("notes route");
   res.json(jsonData)
 });
 
-router.get('/api/notes/:id', (req, res)=>{
+router.get('/notes/:id', (req, res)=>{
   res.json(jsonData[req.params.id]);
   });
 
-router.post("/api/notes", (req, res) => {
+router.post("/notes", (req, res) => {
   let newNote = req.body;
   let noteId = (jsonData.length).toString();
   newNote.id= noteId
@@ -28,22 +28,6 @@ router.post("/api/notes", (req, res) => {
   res.json(jsonData)
 
 });
-
-// router.get("/api/notes", (req, res) => {
-//   console.log("We hit our notes route!");
-//   fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, note) => {
-//     if (err) throw err;
-//     console.log(note);
-//     res.json(JSON.parse(note));
-//   });
-// });
-
-// router.post("/api/notes", (req, res) => {
-//   fs.writeFile(path.join(__dirname, "../db/db.json"), note, (err) => {
-//     if (err) throw err;
-//     console.log("Saved!");
-//   });
-// });
 
 // fs.writeFile(fileName, variable, function (err) {
 //   if (err) throw err;
